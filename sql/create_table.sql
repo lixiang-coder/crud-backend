8ab1ac27c52bb513a650ff145cb46fa3ad9dc4dc
86132 <2894702202@qq.com>
2024-11-18 11:20:04 +0800

    fix:修复了，日志表return_value字段太长而无法存储的bug，并学习一些加密的东西

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -31,3 +31,18 @@ create table operate_log
     return_value  varchar(2000) comment '返回值',
     cost_time     bigint comment '方法执行耗时, 单位:ms'
 ) comment '操作日志表';
+
+INSERT INTO user (name, age, status, sex, address, phone, create_time, update_time)
+SELECT
+    CONCAT('姓名', FLOOR(RAND() * 100)), -- 随机生成姓名
+    FLOOR(RAND() * 80) + 1, -- 随机生成年龄，范围1-80
+    FLOOR(RAND() * 2) + 1, -- 随机生成状态，1有效，0无效
+    CASE FLOOR(RAND() * 2) WHEN 0 THEN '男' ELSE '女' END, -- 随机生成性别
+    CONCAT('地址', FLOOR(RAND() * 100)), -- 随机生成地址
+    CONCAT('电话', LPAD(FLOOR(RAND() * 10000000000), 11, '0')), -- 随机生成电话
+    NOW(), -- 创建时间
+    NOW() -- 修改时间
+FROM
+    (SELECT @rownum:=0) r
+        JOIN
+    (SELECT @rownum:=@rownum+1 AS n FROM information_schema.tables LIMIT 100) t; -- 生成100行
--- src/test/java/com/xzy/crud/CrudApplicationTests.java
+++ src/test/java/com/xzy/crud/CrudApplicationTests.java
@@ -3,11 +3,95 @@ package com.xzy.crud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 @SpringBootTest
 class CrudApplicationTests {
 
+    /**
+     * 加密方法
+     *      缺点：加密后的密文长度并不是固定的，而是取决于明文数据的长度
+     *      优点：加密后，可以实现对密文的解密
+     */
     @Test
-    void contextLoads() {
+    void AesEncryptionExample () throws Exception {
+        // 生成密钥
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256); // 可以选择128、192或256位密钥
+        SecretKey secretKey = keyGenerator.generateKey();
+
+        // 待加密的字符串
+        String plainText = "这是需要加密的返回数据";
+
+        // 加密
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes("UTF-8"));
+        String encryptedText = Base64.getEncoder().encodeToString(encryptedBytes);
+
+
+        // 解密
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        String decryptedText = new String(decryptedBytes, "UTF-8");
+
+        System.out.println("原文: " + plainText);
+        System.out.println("密文: " + encryptedText);
+        System.out.println("解密后: " + decryptedText);
     }
+
+    private static final String KEY_STRING = "0123456789abcdef"; // 16字节的密钥
+    private static final String PADDING_CHAR = "=";
+
+    /**
+     * 我操了，一早上都在研究如何把很长的文本加密成指定长度的字符串，然后这个加密后的密文还可以解密成原文，
+     * 在网上看到条评论说：有这技术都靠卖专利都成首富了，我才恍然大悟。
+     */
+    @Test
+    void FixedLengthAESEncryption() throws Exception{
+        String plainText = "这是需要加密的返回数据";
+        String encryptedText = encrypt(plainText);
+        System.out.println("加密后的字符串: " + encryptedText);
+
+
+        String decryptedText = decrypt(encryptedText);
+        System.out.println("解密后的字符串: " + decryptedText);
+    }
+
+    // 填补
+    public static String pad(String plainText) {
+        int paddingLength = 16 - (plainText.length() % 16);
+        return plainText + PADDING_CHAR.repeat(paddingLength);
+    }
+
+    public static String unpad(String paddedText) {
+        return paddedText.replace(PADDING_CHAR, "");
+    }
+
+    // 加密
+    public static String encrypt(String plainText) throws Exception {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(KEY_STRING.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        byte[] encryptedBytes = cipher.doFinal(pad(plainText).getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    // 解密
+    public static String decrypt(String encryptedText) throws Exception {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(KEY_STRING.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+        return unpad(new String(decryptedBytes));
+    }
+
+
+
 
 }
